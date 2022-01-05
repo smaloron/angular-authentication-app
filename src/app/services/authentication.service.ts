736bd5bcbe4771ca76d92f4b4fecc43958b65615
seb <contact@smaloron.eu>
2022-01-05 12:24:21 +0100

    Ajout de multiples Users

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { User, UserInterface } from '../models/user.model';
+import { User, UserDataInterface, UserInterface } from '../models/user.model';
 
 export interface CredentialInterface {
   login: string,
@@ -13,19 +13,29 @@ export class AuthenticationService {
 
   user: UserInterface;
 
+  userList: UserDataInterface[] = [
+    { login: 'Ada', userName: 'Ada Lovelace', password: '123' },
+    { login: 'Toto', userName: 'Toto le héros', password: '123' },
+    { login: 'Pauline', userName: 'Pauline Martin', password: '123' },
+    { login: 'grace', userName: 'Grace Hopper', password: '456' },
+    { login: 'Alan', userName: 'Alan Turing', password: '456' },
+  ]
+
   constructor() {
     this.user = new User();
-   }
+  }
+  
+  findUser(credentials: CredentialInterface): UserDataInterface | undefined {
+    return this.userList.find(item => item.login == credentials.login && item.password == credentials.password);
+  }
 
   authenticate(credentials: CredentialInterface) {
-    const isAuthenticated = credentials.login === 'user' &&
-      credentials.password === '123';
+    const user = this.findUser(credentials);
+    
+    const isAuthenticated = user != undefined;
     
     if (isAuthenticated) {
-      this.user = new User({
-        userName: 'Defaut User',
-        login: credentials.login
-      });
+      this.user = new User(user);
     }
 
     return isAuthenticated;
