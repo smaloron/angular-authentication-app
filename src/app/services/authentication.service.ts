eccfb85be85d3e930ab7b2855c1c3e3d0a97af82
seb <contact@smaloron.eu>
2022-01-07 10:55:14 +0100

    Authentification persistante

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { User, UserDataInterface, UserInterface } from '../models/user.model';
 import { NotificationService } from './notification.service';
+import { LocalStorageService } from 'ngx-webstorage';
 
 export interface CredentialInterface {
   login: string,
   password: string
 };
+
+const USER_KEY = 'user';
 
 @Injectable({
   providedIn: 'root'
@@ -24,8 +27,15 @@ export class AuthenticationService {
   ]
 
   constructor(private notification: NotificationService,
-              private router: Router) {
-    this.user = new User();
+    private router: Router,
+    private storage: LocalStorageService) {
+    
+    const user = JSON.parse(storage.retrieve(USER_KEY));
+    if (user) {
+      this.user = new User(user);
+    } else {
+      this.user = new User();
+    }
   }
   
   findUser(credentials: CredentialInterface): UserDataInterface | undefined {
@@ -40,13 +50,15 @@ export class AuthenticationService {
     if (isAuthenticated) {
       this.notification.setMessage('Vous êtes connecté');
       this.user = new User(user);
+      this.storage.store(USER_KEY, JSON.stringify(this.user));
     }
-
+  
     return isAuthenticated;
   }
 
   logout(): void {
     this.user = new User();
+    this.storage.clear(USER_KEY);
     this.notification.setMessage('Vous êtes déconnecté');
     this.router.navigate(['/home'])
   }
