0d63236c485326ffa17634a675e14cbe0a2bd1b6
seb <contact@smaloron.eu>
2022-01-07 13:40:39 +0100

    Filtre par genre et nationalité plus nobre de résultats

--- src/app/pages/random-user-list/random-user-list.component.ts
+++ src/app/pages/random-user-list/random-user-list.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-random-user-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RandomUserListComponent implements OnInit {
 
-  constructor() { }
+  readonly url = 'https://randomuser.me/api';
+
+  userList: any[] = [];
+
+  search = {
+    nat: '',
+    gender: '',
+    results: 10
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    const httpParams = new HttpParams()
+      .set('results', this.search.results)
+      .set('nat', this.search.nat)
+      .set('gender', this.search.gender);
+    this.http.get(this.url, {params: httpParams}).subscribe((response: any) => {
+      this.userList = response.results;
+    });
   }
 
 }
