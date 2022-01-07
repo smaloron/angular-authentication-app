c9e1995ef713505091e57f5633928573ad3682ef
seb <contact@smaloron.eu>
2022-01-07 10:55:32 +0100

    Sécurisation de la modification d'une tâche

--- src/app/pages/todo/todo-list/todo-list.component.ts
+++ src/app/pages/todo/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Todo } from 'src/app/models/todo.model';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { NotificationService } from 'src/app/services/notification.service';
 import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
@@ -9,9 +12,20 @@ import { TodoService } from 'src/app/services/todo.service';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor(public taskService: TodoService) { }
+  constructor(public taskService: TodoService,
+              private security: AuthenticationService,
+    private router: Router,
+  private notification: NotificationService) { }
 
   ngOnInit(): void {
+  }
+
+  editTask(task:Todo): void {
+    if (task.user === this.security.user.login) {
+      this.router.navigate(['/todo-form', task.id]);
+    } else {
+      this.notification.setMessage(`Vous ne pouvez pas modifier cette tâche`);
+    }
   }
 
 }
